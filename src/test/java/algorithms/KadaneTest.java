17f771af311d6f0cc1c736aa3959d6b6d7aca154
meaemb <makhanbetiyarb@mail.ru>
2025-10-05 16:43:07 +0300

    test(algorithm): add JUnit tests for Kadane edge cases

--- src/test/java/algorithms/KadaneTest.java
+++ src/test/java/algorithms/KadaneTest.java
@@ -10,31 +10,23 @@ public class KadaneTest {
         assertEquals(0, Kadane.maxSubarray(new int[]{}).sum);
     }
 
-    @Test void single_element(){
+    @Test void single_element_negative(){
         var r = Kadane.maxSubarray(new int[]{-7});
-        assertEquals(-7, r.sum);
-        assertEquals(0, r.left);
-        assertEquals(0, r.right);
+        assertEquals(-7, r.sum); assertEquals(0, r.left); assertEquals(0, r.right);
     }
 
     @Test void all_negative(){
         var r = Kadane.maxSubarray(new int[]{-5,-2,-9});
-        assertEquals(-2, r.sum);
-        assertEquals(1, r.left);
-        assertEquals(1, r.right);
+        assertEquals(-2, r.sum); assertEquals(1, r.left); assertEquals(1, r.right);
     }
 
-    @Test void mix_positive_negative(){
-        var r = Kadane.maxSubarray(new int[]{1, 2, -5, 4, 7, -2});
-        assertEquals(11, r.sum); // 4 + 7
-        assertEquals(3, r.left);
-        assertEquals(4, r.right);
+    @Test void mixed(){
+        var r = Kadane.maxSubarray(new int[]{1,2,-5,4,7,-2});
+        assertEquals(11, r.sum); assertEquals(3, r.left); assertEquals(4, r.right);
     }
 
     @Test void zeros(){
         var r = Kadane.maxSubarray(new int[]{0,0,0});
-        assertEquals(0, r.sum);
-        assertEquals(0, r.left);
-        assertEquals(0, r.right);
+        assertEquals(0, r.sum); assertEquals(0, r.left); assertEquals(0, r.right);
     }
 }
